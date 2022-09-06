6a57ce716ef1f42b3901a81e8cf784c53a0f0d1e
nijakow <79372954+nijakow@users.noreply.github.com>
2022-09-06 15:42:45 +0200

    Added condvars

---
+++ src/pal/pthread/condvar.hpp
@@ -0,0 +1,26 @@
+#ifndef WEBSERV_PAL_PTHREAD_CONDVAR_HPP
+#define WEBSERV_PAL_PTHREAD_CONDVAR_HPP
+
+#include "../../defs.hpp"
+#include "mutex.hpp"
+
+namespace webserv {
+    namespace pal {
+        namespace pthread {
+
+    class condvar : mutex {
+        pthread_cond_t   the_cond_var;
+    
+    public:
+        condvar();
+        ~condvar();
+
+        void wait();
+        void signal();
+    };
+
+        }
+    }
+}
+
+#endif
--- src/threading/mutex.hpp
+++ src/threading/mutex.hpp
@@ -2,11 +2,13 @@
 #define WEBSERV_THREADING_MUTEX_HPP
 
 #include "../pal/pthread/mutex.hpp"
+#include "../pal/pthread/condvar.hpp"
 
 namespace webserv {
     namespace threading {
 
-    typedef webserv::pal::pthread::mutex mutex;
+    typedef webserv::pal::pthread::mutex   mutex;
+    typedef webserv::pal::pthread::condvar condvar;
 
     }
 }
