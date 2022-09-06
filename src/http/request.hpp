588668f372da67e564ef20b630fdb46c130fc801
nijakow <79372954+nijakow@users.noreply.github.com>
2022-09-06 20:51:10 +0200

    Added http/request.hpp

---
+++ src/http/request.hpp
@@ -0,0 +1,19 @@
+#ifndef WEBSERV_HTTP_REQUEST_HPP
+#define WEBSERV_HTTP_REQUEST_HPP
+
+namespace webserv {
+    namespace http {
+
+        /*
+         * This class describes a full HTTP request.
+         */
+        class request {
+        private:
+        public:
+        };
+
+    }
+}
+
+
+#endif
