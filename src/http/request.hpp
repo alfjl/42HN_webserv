d60d59bda48b6fe1665c10965b5cecd95f0c54f8
nijakow <79372954+nijakow@users.noreply.github.com>
2022-09-06 21:11:10 +0200

    Added http/request

--- src/http/request.hpp
+++ src/http/request.hpp
@@ -5,9 +5,53 @@ namespace webserv {
     namespace http {
 
         /*
-         * This class describes a full HTTP request.
+         * This section implements an HTTP request line, as described in:
+         *
+         *     https://www.rfc-editor.org/rfc/rfc2616#section-5.1
          */
-        class request {
+
+        enum request_method {
+            // request_method_options,
+            request_method_get,
+            // request_method_head,
+            request_method_post,
+            // request_method_put,
+            request_method_delete,
+            // request_method_trace,
+            // request_method_connect
+        };
+
+        class uri {
+            /* TODO */
+        };
+
+        typedef uri request_uri;
+
+        class http_version {
+            /* TODO */
+        };
+
+        class request_line {
+        private:
+            enum request_method _method;
+            request_uri         _uri;
+            http_version        _http_version;
+        
+        public:
+
+        };
+
+
+        /*
+         * This class implements an HTTP request core, as described in:
+         *
+         *     https://www.rfc-editor.org/rfc/rfc2616#section-5
+         * 
+         * More specifically, this class leaves out the message body, as it
+         * may be delivered in a chunked format.
+         *                                       - nijakow
+         */
+        class request_core {
         private:
         public:
         };
