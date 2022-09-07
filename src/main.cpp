777f63f949d7bb5498d6bd250931f6757c02f642
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-09-07 16:07:43 +0200

    Add classes guarded and borrow to pal

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 
 void test_thread_pool();
 void test_shared_ptr ();
+void test_guarded_borrow ();
 
 int main(int argc, char *argv[]) {
     std::cout << "Hello, 42 world!" << std::endl;
     test_shared_ptr ();
-    test_thread_pool();
+    test_guarded_borrow ();
+    // test_thread_pool();
     return 0;
 }
