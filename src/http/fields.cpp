2c6414487cd6648729ae6af8412aa5624bc25d4f
nijakow <79372954+nijakow@users.noreply.github.com>
2022-09-07 13:55:22 +0200

    Added http fields

---
+++ src/http/fields.cpp
@@ -0,0 +1,20 @@
+#include "fields.hpp"
+
+namespace webserv {
+    namespace http {
+
+        fields::fields() {}
+        fields::~fields() {}
+
+        std::string fields::get_or_default(std::string key, std::string deflt) const {
+            std::map<std::string, std::string>::const_iterator it = _fields.find(key);
+            if (it == _fields.end()) return deflt;
+            else                     return it->second;
+        }
+
+        void fields::put(std::string key, std::string value) {
+            _fields[key] = value;
+        }
+
+    }
+}
---
+++ src/http/fields.hpp
@@ -0,0 +1,24 @@
+#ifndef WEBSERV_HTTP_FIELDS_HPP
+#define WEBSERV_HTTP_FIELDS_HPP
+
+#include "../defs.hpp"
+
+namespace webserv {
+    namespace http {
+
+        class fields {
+        private:
+            std::map<std::string, std::string> _fields;
+        
+        public:
+            fields();
+            ~fields();
+
+            std::string get_or_default(std::string key, std::string deflt) const;
+            void        put(std::string key, std::string value);
+        };
+
+    }
+}
+
+#endif
