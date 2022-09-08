0a1d05ab05e76fffe7cb5fc6ac190c65c720617c
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-09-08 18:49:12 +0200

    Add both for loops in 2 versions each

--- src/pal/net/selector.hpp
+++ src/pal/net/selector.hpp
@@ -44,32 +44,77 @@ namespace webserv {
         void select() {}
 
             int     highest_fd = -1;
-            fd_set  readable;
-            fd_set  writeable;
-            fd_set  except;
+            fd_set  read_fds;
+            fd_set  write_fds;
+            fd_set  exception_fds;
 
-            the_mutex.lock();
-            // iterate over all sockets and save into readable, writable or expect exception
+            FD_ZERO(&read_fds);
+            FD_ZERO(&write_fds);
+            FD_ZERO(&exception_fds);
+
+            // ------------------------ 1. loop --------------------------------
+            // iterate over all sockets and save into read_fds, writable or expect exception
             std::map<socket *, payload_type>::iterator it = elements.begin();
             std::map<socket *, payload_type>::iterator ite = elements.end();
+            the_mutex.lock();
             for ( ; it != ite; ++it) {
-                if (socket.is_server_socket()) {
-
+                if (it.is_server_socket()) {
+                    FD_SET(it.get_fd(), /* fill in correct fd_set here */);
                 }
-                else if (socket.is_data_socket()) {
-
+                else if (it.is_data_socket()) {
+                    FD_SET(it.get_fd(), /* fill in correct fd_set here */);
                 }
+                FD_SET(it.get_fd(), exception_fds);
                 if (it.get_fd() > highest_fd)
                     highest_fd = it.get_fd();
             }
             the_mutex.unlock();
 
-            int status = ::select(highest_fd + 1, readable, writabel, except, NULL);
+            // // or:
+            // the_mutex.lock();
+            // for ( ; it != ite; ++it) {
+            //     FD_SET(it.get_fd(), read_fds);
+            //     FD_SET(it.get_fd(), write_fds);
+            //     FD_SET(it.get_fd(), exception_fds);
+            //     if (it.get_fd() > highest_fd)
+            //         highest_fd = it.get_fd();
+            // }
+            // the_mutex.unlock();
+            // ------------------------ 1. loop --------------------------------
+
+
+            // use ::select() to check all 3 
+            int status = ::select(highest_fd + 1, read_fds, write_fds, exception_fds, NULL);
             if (status < 0) // if select() throws error
                 throw std::runtime_error("select(...) returned an error code!");
             // TODO: Question:
             // "::select() returns 0 if the time limit expires"
             // catch in same exception as '-1' error, or ignore?
+
+
+            // ------------------------ 2. loop --------------------------------
+            // iterate over all sockets and check, which fd_set they belong to
+            it = elements.begin();
+            for ( ; it != ite; ++it) {
+                if (!FD_ISSET(it.get_fd(), exception_fds))
+                    // do_socket_operation(); TODO: What to do with that information?
+            }
+            for (int i = 0; i < exception_fds.fd_ount; ++i)
+                // throw exception? TODO: What to do in that case?
+
+            // // or:
+            // it = elements.begin();
+            // for ( ; it != ite; ++it) {
+            //     if (FD_ISSET(it.get_fd(), read_fds))
+            //         // do_read_operation(); TODO: What to do with that information?
+            //     else if (FD_ISSET(it.get_fd(), write_fds))
+            //         // do_write_operation(); TODO: What to do with that information?
+            //     else if (FD_ISSET(it.get_fd(), exception_fds))
+            //         // do_exception_operation(); TODO: What to do with that information?
+            // }
+            // ------------------------ 2. loop --------------------------------
+            // TODO: check
+            // TODO: think about, if we have to lock or unlock second for loop
         }
 
         /* TODO: Adding, removing, selecting, yada yada yada ... */
