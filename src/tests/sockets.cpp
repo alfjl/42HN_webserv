269ea4db91040b5a16249d824ef615fd1cfaf105
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-09-09 12:25:25 +0200

    Add print statments for sockets 5 and 6

--- src/tests/sockets.cpp
+++ src/tests/sockets.cpp
@@ -9,12 +9,6 @@ struct screamer {
   ~screamer() { std::cout << "Destroying screamer " << this << std::endl; }
 };
 
-std::ostream& operator<<(std::ostream& o, const screamer& scream)
-{
-  o << &scream;
-  return (o);
-}
-
 void test_sockets () {
 
  //TODO: test with different threads
@@ -22,7 +16,6 @@ void test_sockets () {
 int fd10(10);
 int fd50(50);
 int fd60(60);
-screamer screamer1;
 
 webserv::pal::net::socket s1;
 webserv::pal::net::socket s2(fd10);
@@ -30,6 +23,14 @@ webserv::pal::net::server_socket server_s3;
 webserv::pal::net::data_socket data_s4;
 webserv::pal::net::server_socket server_s5(fd50);
 webserv::pal::net::data_socket data_s6(fd60);
+
+std::cout << "s1.fd = " << s1.get_fd() << std::endl;
+std::cout << "s2.fd = " << s2.get_fd() << std::endl;
+std::cout << "server_s3.fd = " << server_s3.get_fd() << std::endl;
+std::cout << "data_s4.fd = " << data_s4.get_fd() << std::endl;
+std::cout << "server_s5.fd = " << server_s5.get_fd() << std::endl;
+std::cout << "data_s6.fd = " << data_s6.get_fd() << std::endl;
+std::cout << std::endl;
 
 s1.set_non_blocking();
 s2.set_non_blocking();
@@ -40,9 +41,15 @@ std::cout << "s1.fd = " << s1.get_fd() << std::endl;
 std::cout << "s2.fd = " << s2.get_fd() << std::endl;
 std::cout << "server_s3.fd = " << server_s3.get_fd() << std::endl;
 std::cout << "data_s4.fd = " << data_s4.get_fd() << std::endl;
+std::cout << "server_s5.fd = " << server_s5.get_fd() << std::endl;
+std::cout << "data_s6.fd = " << data_s6.get_fd() << std::endl;
+std::cout << std::endl;
 
-
-
-
+s1.close();
+s2.close();
+server_s3.close();
+data_s4.close();
+server_s5.close();
+data_s6.close();
 
 }
