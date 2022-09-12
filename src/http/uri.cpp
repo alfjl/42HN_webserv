411c88b73b56578a5d8c9c9684b7ed0da450367c
enijakow <enijakow@student.42heilbronn.de>
2022-09-12 12:13:47 +0200

    Implemented URIs

--- src/http/uri.cpp
+++ src/http/uri.cpp
@@ -1,2 +1,14 @@
 #include "uri.hpp"
 
+namespace webserv {
+    namespace http {
+
+        uri::uri() : _proto("http"), _server(""), _port(80), _path() {
+
+        }
+
+        uri::~uri() {
+
+        }
+    }
+}
--- src/http/uri.hpp
+++ src/http/uri.hpp
@@ -1,11 +1,27 @@
 #ifndef WEBSERV_HTTP_URI_HPP
 #define WEBSERV_HTTP_URI_HPP
 
+#include "../defs.hpp"
+
 namespace webserv {
     namespace http {
 
+        class path {
+        };
+
         class uri {
-            /* TODO */
+            std::string   _proto;
+            std::string   _server;
+            unsigned int  _port;
+            path          _path;
+        public:
+            uri();
+            ~uri();
+
+            std::string get_proto() { return _proto; }
+            std::string get_server() { return _server; }
+            unsigned int get_port() { return _port; }
+            path& get_path() { return _path; }
         };
 
     }
