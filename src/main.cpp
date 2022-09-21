08f93e99d949c7056ccd40fae6cb1e39f912df5b
Nils Lenoch <nlenoch@2-A-6.42heilbronn.de>
2022-09-21 14:25:32 +0200

    Flows implemented

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 
 void test_sockets();
 void test_uri_parsing();
-void test_streamflow();
 
 int main(int argc, char *argv[]) {
     std::cout << "\nHello, 42 world!" << std::endl;
@@ -10,7 +9,5 @@ int main(int argc, char *argv[]) {
     test_sockets();
     std::cout << "\n --------- URI PARSING --------- \n" << std::endl;
     test_uri_parsing();
-    std::cout << "\n --------- STREAMFLOW --------- \n" << std::endl;
-    test_streamflow();
     return 0;
 }
