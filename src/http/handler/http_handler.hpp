38b3705d2d19cb286a11ac219082db062f29f027
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-09-23 14:24:10 +0200

    Commit before checkout

--- src/http/handler/http_handler.hpp
+++ src/http/handler/http_handler.hpp
@@ -72,66 +72,6 @@ namespace webserv {
                 }
             }
 
-            // void start() {
-            //     next(&wait_for_char);
-            //     later(&process_char);
-            // }
-
-            // void end() {
-            //     if (keepalive) {
-            //         next(&start);
-            //     } else {
-            //         close_connection();
-            //     }
-            // }
-
-            // void wait_for_char() {
-            //     if (!connection.has()) { 
-            //         yield(&wait_for_char);
-            //         return; // Give control back to multiplexer
-            //     }
-            //     last_char = connection.next_char();
-            //     ret();
-            // }
-
-            // void process_char() {
-            //     buffer.append(last_char);
-            //     size_t pos = buffer.find("\r\n\r\n");
-            //     if (pos != std::string::npos) {
-            //         next(&parse_header_and_decide);
-            //     } else {
-            //         next(wait_for_char);
-            //         later(&process_char);
-            //     }
-            // }
-
-            // void parse_header_and_decide() {
-            //     // TODO: Parse header from buffer
-            //     if (expects_body()) {
-            //         if (is_chunked()) {
-            //             next(&parse_chunked_body);
-            //         } else {
-            //             next(&parse_normal_body);
-            //         }
-            //     }
-            //     later(&process_request);
-            // }
-
-            // void parse_chunked_body() {
-            //     // TODO
-            //     ret();
-            // }
-
-            // void parse_normal_body() {
-            //     // TODO
-            //     ret();
-            // }
-
-            // void process_request() {
-            //     // TODO
-            //     ret();
-            // }
-
         }; // class http_handler
 
     } // namespace http
