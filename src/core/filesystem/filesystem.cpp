1691fdcbdef6f2479024526cd4d178f269394524
nijakow <79372954+nijakow@users.noreply.github.com>
2022-09-28 13:17:09 +0200

    Implemented filesystem::open_absolute(...)

--- src/core/filesystem/filesystem.cpp
+++ src/core/filesystem/filesystem.cpp
@@ -3,13 +3,17 @@
 namespace webserv {
 	namespace core {
 
-	filesystem::filesystem(instance& the_inst) : component(the_inst){
+		filesystem::filesystem(instance& the_inst) : component(the_inst){
 
-	}
+		}
 
-	filesystem::~filesystem() {
+		filesystem::~filesystem() {
 
-	}
+		}
+
+		std::ifstream open_absolute(webserv::http::path path) {
+			return std::ifstream("/" + path.get_addr_s());
+		}
 
 	} // namespace core
 } // namespace webserv
