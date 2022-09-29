004f81f6175084d7a443aaf74cd3f94dd7dd03a5
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-09-29 10:50:31 +0200

    Initial commit: Generate dir/readdir.hpp & .cpp

---
+++ src/pal/dir/readdir.hpp
@@ -0,0 +1,18 @@
+#ifndef WEBSERV_PAL_DIR_READDIR_HPP
+#define WEBSERV_PAL_DIR_READDIR_HPP
+
+#include "../../defs.hpp"
+
+namespace webserv {
+    namespace pal {
+        namespace dir {
+
+            class readdir {
+
+            }; // class readdir
+
+        } // namespace dir
+    } // namespace pal
+} // namespace webserv
+
+#endif
