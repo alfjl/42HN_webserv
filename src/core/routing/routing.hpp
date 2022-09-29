38080377d81ec0a721a1c02c1666de7030e6cf3a
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-09-29 16:03:41 +0200

    Add error_code()

--- src/core/routing/routing.hpp
+++ src/core/routing/routing.hpp
@@ -23,6 +23,19 @@ namespace webserv {
 
             void tick();
 
+        protected:
+            // error codes
+            void error_code(webserv::http::response_fixed* response, unsigned int code);
+            void permanent_redirect_301(webserv::http::response_fixed* response);
+            void temporary_redirect_302(webserv::http::response_fixed* response);
+            void bad_request_400(webserv::http::response_fixed* response);
+            void unauthorized_401(webserv::http::response_fixed* response);
+            void not_found_404(webserv::http::response_fixed* response);
+            void gone_410(webserv::http::response_fixed* response);
+            void teapot_418(webserv::http::response_fixed* response);
+            void internal_server_error_500(webserv::http::response_fixed* response);
+            void service_unavailable_503(webserv::http::response_fixed* response);
+
         }; // class routing
 
     } // namespace core
