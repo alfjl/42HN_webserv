854496b1e7811aa28708ef5107e9790c00bb5526
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-09-29 13:20:22 +0200

    Moving from class readdir, to function read_directory(): Deleting readdir.hpp; Writing read_directory()

--- src/pal/dir/readdir.cpp
+++ src/pal/dir/readdir.cpp
@@ -1,13 +1,27 @@
-#include "readdir.hpp"
+#include <dirent.h>
+
+#include "../../defs.hpp"
 
 namespace webserv {
     namespace pal {
         namespace dir {
 
-            std::vector<std::string> readdir::readdir_r(const std::string& dir) {
+            std::vector<std::string> read_directory(const std::string& path) {
                 std::vector<std::string> files;
-
-                // TODOD; Impelment!
+                DIR*                     dir_ptr;
+                struct dirent*           entry;
+                
+                dir_ptr = opendir(path.c_str());
+                // if (!dir_ptr) {
+                //     // throw exception? Or how to handle the error?
+                // }
+                if (dir_ptr) {
+                    while ((entry = readdir(dir_ptr)) != NULL) {
+                        if (!(entry->d_name == ".") && !(entry->d_name == "..")) {
+                            files.push_back(entry->d_name);
+                        }
+                    }
+                }
 
                 return files;
             }
