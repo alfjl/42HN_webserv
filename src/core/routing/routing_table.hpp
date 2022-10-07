867f2364ca2cd968f38ebccba365df7d639f3a3e
Nils Lenoch <nlenoch@2-A-6.42heilbronn.de>
2022-10-07 15:54:52 +0200

    refactoring routing-table

--- src/core/routing/routing_table.hpp
+++ src/core/routing/routing_table.hpp
@@ -13,13 +13,13 @@ namespace webserv {
             typedef std::vector<std::pair<webserv::util::path, webserv::util::path> >::iterator       iterator;
             typedef std::vector<std::pair<webserv::util::path, webserv::util::path> >::const_iterator const_iterator;
             
-            std::vector<std::pair<webserv::core::basic_rule*, webserv::core::route*> >   prefix_rules;
+            std::vector<std::pair<webserv::core::basic_rule*, webserv::core::route> >   prefix_rules;
         
         public:
             routing_table() {}
-            ~routing_table() {}
+            ~routing_table();
 
-            void add_rule(webserv::core::basic_rule* in, webserv::core::route* out);
+            void add_rule(webserv::core::basic_rule* in, webserv::core::route out);
             route query(webserv::util::path path);
         };
 
