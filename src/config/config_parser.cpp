f5f645135f311678625e6b2e859f188da08da85e
Nils Lenoch <nlenoch@2-A-6.42heilbronn.de>
2022-10-10 18:29:19 +0200

    first test works

--- src/config/config_parser.cpp
+++ src/config/config_parser.cpp
@@ -15,7 +15,7 @@ namespace webserv {
 		std::string config_parser::read_word() {
 			std::string word;
 
-			skip_spaces();
+			skip_whitespace();
 			while (true) {
 				char c;
 				     if (!this_char(c)) break;
@@ -39,17 +39,23 @@ namespace webserv {
 			return 0;
 		}
 
+		void config_parser::skip_whitespace() {
+			while (check(' ') || check('\t') || check('\r') || check('\n'));
+		}
+
 		void config_parser::run() {
 			// start
-			skip_spaces();
+			skip_whitespace();
 			expects("server");
-			skip_spaces();
+			skip_whitespace();
 			expects("{");
+			skip_whitespace();
 			while(!checks("}")){
-			
+				skip_whitespace();
 				if (checks("listen")) {
-					skip_spaces();
+					skip_whitespace();
 					read_int();
+					skip_whitespace();
 				} else if (checks("server_name") || checks("access_log")) {
 					while (!checks(";")){
 						read_word();
@@ -57,11 +63,11 @@ namespace webserv {
 				} else if (checks("root")) {
 					read_word();
 				} else if (checks("location")) {
-					skip_spaces();
+					skip_whitespace();
 					read_path();
-					skip_spaces();
+					skip_whitespace();
 					expects("{");
-					skip_spaces();
+					skip_whitespace();
 					while (!checks("}")){
 						if (checks("root")){
 							read_word();
@@ -70,10 +76,15 @@ namespace webserv {
 						} else if (checks("fastcgi_pass")){
 							read_word();
 						}
+						skip_whitespace();
+						expects(";");
+						skip_whitespace();
 					}
 					continue ;
 				}
+				skip_whitespace();
 				expects(";");
+				skip_whitespace();
 			}
 			// end
 		}
