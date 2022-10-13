082637ccc2d55f6c9bcab9ad2219d643d8e4ad53
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-10-13 15:39:21 +0200

    set listen() to 128

--- src/http/cgi/cgi_msg.cpp
+++ src/http/cgi/cgi_msg.cpp
@@ -22,23 +22,23 @@ namespace webserv {
         }
 
         void cgi_message::setup_fields() {
-            _fields.put("AUTH_TYPE", "");
-            _fields.put("CONTENT_LENGTH", (int) _message_body.size());
-            _fields.put("CONTENT_TYPE", _fields.get_or_default("CONTENT_TYPE", "plain"));
-            _fields.put("GATEWAY_INTERFACE", "CGI/1.1");
-            _fields.put("PATH_INFO", "");  // TODO
-            _fields.put("PATH_TRANSLATED", ""); // TODO
-            _fields.put("QUERY_STRING", "");  // TODO
-            _fields.put("REMOTE_ADDR", "127.0.0.1");  // TODO: localhost
-            _fields.put("REMOTE_HOST", "");
-            _fields.put("REMOTE_IDENT", ""); // Not really needed
-            _fields.put("REMOTE_USER", "");
-            _fields.put("REQUEST_METHOD", _method);
-            _fields.put("SCRIPT_NAME", "");
-            _fields.put("SERVER_NAME", "");
-            _fields.put("SERVER_PORT", "");
-            _fields.put("SERVER_PROTOCOL", "");
-            _fields.put("SERVER_SOFTWARE", "");
+            // _fields.put("AUTH_TYPE", "");
+            // _fields.put("CONTENT_LENGTH", (int) _message_body.size());
+            // _fields.put("CONTENT_TYPE", _fields.get_or_default("CONTENT_TYPE", "plain"));
+            // _fields.put("GATEWAY_INTERFACE", "CGI/1.1");
+            // _fields.put("PATH_INFO", "");  // TODO
+            // _fields.put("PATH_TRANSLATED", ""); // TODO
+            // _fields.put("QUERY_STRING", "");  // TODO
+            // _fields.put("REMOTE_ADDR", "127.0.0.1");  // TODO: localhost
+            // _fields.put("REMOTE_HOST", "");
+            // _fields.put("REMOTE_IDENT", ""); // Not really needed
+            // _fields.put("REMOTE_USER", "");
+            // _fields.put("REQUEST_METHOD", _method);
+            // _fields.put("SCRIPT_NAME", "");
+            // _fields.put("SERVER_NAME", ""); // = HOST cut until :
+            // _fields.put("SERVER_PORT", ""); // = HOST: alles nach :
+            // _fields.put("SERVER_PROTOCOL", "");
+            // _fields.put("SERVER_SOFTWARE", "");
         }
 
         void cgi_message::write_on(std::ostream& o) {
