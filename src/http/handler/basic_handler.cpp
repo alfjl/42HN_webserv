e06a16492864c70cf1af8cf556d21a830bd22d09
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-10-27 12:54:14 +0200

    Push function definitions from basic_handler.hpp to .cpp

--- src/http/handler/basic_handler.cpp
+++ src/http/handler/basic_handler.cpp
@@ -9,6 +9,11 @@
 namespace webserv {
     namespace http {
 
+        /*
+         *
+         *     C o n s t r u c t o r s ,   G e t t e r s   a n d   S e t t e r s
+         *
+         */
         basic_handler::basic_handler(webserv::util::connection* new_connection)
             : _connection(new_connection) {
             if (_connection != NULL)
@@ -26,6 +31,13 @@ namespace webserv {
         webserv::util::connection* basic_handler::get_connection() { return _connection; }
         struct connection_config*  basic_handler::get_connection_configs() { return &_connection_configs; }
 
+
+
+        /*
+         *
+         *     S t a t e   M a c h i n e   F u n c t i o n s
+         *
+         */
         void basic_handler::read_next_char() {
             if (in().has_next()) {
                 char c;
@@ -43,6 +55,100 @@ namespace webserv {
             }
         }
 
+                void basic_handler::read_fields() {
+                    later(&basic_handler::parse_fields);
+                    later(&basic_handler::read_until_rnrn);
+                }
+
+                    void basic_handler::read_until_rnrn() {
+                        _read_until_rnrn__buffer = "";
+                        later(&basic_handler::read_until_rnrn__restart);
+                    }
+
+                        void basic_handler::read_until_rnrn__restart() {
+                            later(&basic_handler::read_until_rnrn__continue);
+                            later(&basic_handler::read_until_rn);
+                        }
+
+                        void basic_handler::read_until_rnrn__continue() {
+                            if (_read_until_rn__buffer != "") {
+                                _read_until_rnrn__buffer += _read_until_rn__buffer;
+                                _read_until_rnrn__buffer += "\r\n";
+                                later(&basic_handler::read_until_rnrn__restart);
+                            } else {
+                                _read_until_rnrn__buffer += "\r\n";
+                                // This "function" returns here: Do nothing!
+                                return;
+                            }
+                        }
+
+                        void basic_handler::read_until_rn() {
+                            _read_until_rn__buffer = "";
+                            later(&basic_handler::read_until_rn__restart);
+                        }
+
+                            void basic_handler::read_until_rn__restart() {
+                                later(&basic_handler::read_until_rn__continue);
+                                later(&basic_handler::read_next_char);
+                            }
+
+                            void basic_handler::read_until_rn__continue() {
+                                if (_last_char.enabled()) {
+                                    _read_until_rn__buffer += _last_char.value();
+                                    if (_read_until_rn__buffer.find("\r\n") != std::string::npos) {
+                                        // We return: Do nothing!
+                                        _read_until_rn__buffer = _read_until_rn__buffer.substr(0, _read_until_rn__buffer.size() - 2);
+                                        return;
+                                    } else {
+                                        later(&basic_handler::read_until_rn__restart);
+                                    }
+                                } else {
+                                    // We return: Do nothing!
+                                    return;
+                                }
+                            }
+
+                    void basic_handler::read_normal_body() {
+                        _read_normal_body__result = "";
+                        later(&basic_handler::read_normal_body__restart);
+                    }
+
+                        void basic_handler::read_normal_body__restart() {
+                            if (_read_normal_body__expected_size > 0) {
+                                later(&basic_handler::read_normal_body__continue);
+                                later(&basic_handler::read_next_char);
+                            } else {
+                                // This "function" returns here: Do nothing!
+                                return;
+                            }
+                        }
+
+                        void basic_handler::read_normal_body__continue() {
+                            if (_last_char.enabled()) {
+                                _read_normal_body__expected_size--;
+                                _read_normal_body__result += _last_char.value();
+                                later(&basic_handler::read_normal_body__restart);
+                            } else {
+                                // This "function" returns here: Do nothing!
+                                return;
+                            }
+                        }
+
+                    void basic_handler::read_chunked_body() {
+                        _read_chunked_body__result = "";
+                        later(&basic_handler::read_chunked_body__restart);
+                    }
+
+                        void basic_handler::read_chunked_body__restart() {
+                            later(&basic_handler::read_chunked_body__parse_hex);
+                            later(&basic_handler::read_until_rn);
+                        }
+
+                        void basic_handler::read_chunked_body__continue() {
+                            // TODO: Check how many bytes we have actually read
+                            _read_chunked_body__result += _read_normal_body__result;
+                            later(&basic_handler::read_chunked_body__restart);
+                        }
 
         void basic_handler::perform_abort() {
             if (abort() == abort_mode_terminate)
--- src/http/handler/basic_handler.hpp
+++ src/http/handler/basic_handler.hpp
@@ -66,106 +66,38 @@ namespace webserv {
 
             void read_next_char();
 
-                void read_fields() {
-                    later(&basic_handler::parse_fields);
-                    later(&basic_handler::read_until_rnrn);
-                }
+                void read_fields();
 
-                    void read_until_rnrn() {
-                        _read_until_rnrn__buffer = "";
-                        later(&basic_handler::read_until_rnrn__restart);
-                    }
+                    void read_until_rnrn();
 
-                        void read_until_rnrn__restart() {
-                            later(&basic_handler::read_until_rnrn__continue);
-                            later(&basic_handler::read_until_rn);
-                        }
+                        void read_until_rnrn__restart();
 
-                        void read_until_rnrn__continue() {
-                            if (_read_until_rn__buffer != "") {
-                                _read_until_rnrn__buffer += _read_until_rn__buffer;
-                                _read_until_rnrn__buffer += "\r\n";
-                                later(&basic_handler::read_until_rnrn__restart);
-                            } else {
-                                _read_until_rnrn__buffer += "\r\n";
-                                // This "function" returns here: Do nothing!
-                                return;
-                            }
-                        }
+                        void read_until_rnrn__continue();
 
                     virtual void parse_fields() = 0;
 
-                        void read_until_rn() {
-                            _read_until_rn__buffer = "";
-                            later(&basic_handler::read_until_rn__restart);
-                        }
+                        void read_until_rn();
 
-                            void read_until_rn__restart() {
-                                later(&basic_handler::read_until_rn__continue);
-                                later(&basic_handler::read_next_char);
-                            }
+                            void read_until_rn__restart();
 
-                            void read_until_rn__continue() {
-                                if (_last_char.enabled()) {
-                                    _read_until_rn__buffer += _last_char.value();
-                                    if (_read_until_rn__buffer.find("\r\n") != std::string::npos) {
-                                        // We return: Do nothing!
-                                        _read_until_rn__buffer = _read_until_rn__buffer.substr(0, _read_until_rn__buffer.size() - 2);
-                                        return;
-                                    } else {
-                                        later(&basic_handler::read_until_rn__restart);
-                                    }
-                                } else {
-                                    // We return: Do nothing!
-                                    return;
-                                }
-                            }
+                            void read_until_rn__continue();
 
-                    void read_normal_body() {
-                        _read_normal_body__result = "";
-                        later(&basic_handler::read_normal_body__restart);
-                    }
+                    void read_normal_body();
 
-                        void read_normal_body__restart() {
-                            if (_read_normal_body__expected_size > 0) {
-                                later(&basic_handler::read_normal_body__continue);
-                                later(&basic_handler::read_next_char);
-                            } else {
-                                // This "function" returns here: Do nothing!
-                                return;
-                            }
-                        }
+                        void read_normal_body__restart();
 
-                        void read_normal_body__continue() {
-                            if (_last_char.enabled()) {
-                                _read_normal_body__expected_size--;
-                                _read_normal_body__result += _last_char.value();
-                                later(&basic_handler::read_normal_body__restart);
-                            } else {
-                                // This "function" returns here: Do nothing!
-                                return;
-                            }
-                        }
+                        void read_normal_body__continue();
 
-                    void read_chunked_body() {
-                        _read_chunked_body__result = "";
-                        later(&basic_handler::read_chunked_body__restart);
-                    }
+                    void read_chunked_body();
 
-                        void read_chunked_body__restart() {
-                            later(&basic_handler::read_chunked_body__parse_hex);
-                            later(&basic_handler::read_until_rn);
-                        }
+                        void read_chunked_body__restart();
 
                         virtual void read_chunked_body__parse_hex() = 0;
 
-                        void read_chunked_body__continue() {
-                            // TODO: Check how many bytes we have actually read
-                            _read_chunked_body__result += _read_normal_body__result;
-                            later(&basic_handler::read_chunked_body__restart);
-                        }
+                        void read_chunked_body__continue();
 
             virtual enum abort_mode abort() = 0;
+
             void perform_abort();
 
             void total_failure();
