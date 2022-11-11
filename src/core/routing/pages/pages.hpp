00580e06aff79398fc63a70bf14d314e44e1b6d6
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-11-11 17:40:40 +0100

    Remove unnecessary error code functions

--- src/core/routing/pages/pages.hpp
+++ src/core/routing/pages/pages.hpp
@@ -9,14 +9,7 @@ namespace webserv {
 		void error_code(webserv::http::response_fixed& response, unsigned int code);
 		void permanent_redirect_301(webserv::http::response_fixed& response, webserv::util::path path);
 		void temporary_redirect_302(webserv::http::response_fixed& response, webserv::util::path path);
-		void bad_request_400(webserv::http::response_fixed& response);
-		void unauthorized_401(webserv::http::response_fixed& response);
-		void not_found_404(webserv::http::response_fixed& response);
-		void method_not_allowed_405(webserv::http::response_fixed& response);
-		void gone_410(webserv::http::response_fixed& response);
-		void teapot_418(webserv::http::response_fixed& response);
 		void internal_server_error_500(webserv::http::response_fixed& response);
-		void service_unavailable_503(webserv::http::response_fixed& response);
 
 		void head_start(std::ostringstream& ost, std::string s);
 		void header_one(std::ostringstream& ost, std::string s);
